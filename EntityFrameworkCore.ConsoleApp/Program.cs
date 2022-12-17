2793f8e616555c091594b4c4bdbdc6cd9f863d94
LAPTOP-5DSTQPA3\workb <sho@synicsw.com>
2022-12-17 15:21:46 -0800

    Add project files.

---
+++ EntityFrameworkCore.ConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EntityFrameworkCore.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ EntityFrameworkCore.Data/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace EntityFrameworkCore.Data
+{
+    public class Class1
+    {
+
+    }
+}
