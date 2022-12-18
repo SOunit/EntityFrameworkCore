8688a05ef82153a746ab991fe3adf6175eeff101
LAPTOP-5DSTQPA3\workb <sho@synicsw.com>
2022-12-17 18:41:04 -0800

    add db-context and connection string

---
+++ EntityFrameworkCore.Data/FootballLeageDbContext.cs
@@ -0,0 +1,22 @@
+﻿using EntityFrameworkCore.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkCore.Data
+{
+    internal class FootballLeageDbContext: DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB; Initial Catalog=FootballLeage_EfCore");
+        }
+
+        public DbSet<Team> Teams { get; set; }
+        public DbSet<League> Leagues { get; set; }
+    }
+}
--- summary.md
+++ summary.md
@@ -6,3 +6,20 @@
   - `TableId` and `Table`
   - StudentId
   - Student
+
+# steps to use EntityFramework
+
+- create db classes
+- install `EntityFrameworkCore.SqlServer`
+- create `dbContext` class
+
+  - add project reference to Data
+    - right click on Dependencies
+    - add project reference
+  - override configuration
+  - setup db-connection-string
+    - to pass connection data to config file
+
+- use local db
+  - SQL Server Object Explorer
+    - open from window tab
